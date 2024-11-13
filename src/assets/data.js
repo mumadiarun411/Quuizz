4b8ba283f1d9ce21006ba15dcbfa481a5fe6d9da
Arun Kumar <mumadiarun411@gmail.com>
2024-11-13 21:18:48 +0530

    yes iam

---
+++ src/assets/data.js
@@ -0,0 +1,63 @@
+export const data=[
+{
+    question:"Which device is required for the Internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1,
+
+},
+
+
+{
+    question:"Which of the following is NOT an anti-virus software ?",
+    option1: "Avast",
+    option2: "Linux",
+    option3: "Norton",
+    option4: "Kaspersky",
+    ans: 2,
+
+},
+
+{
+    question:"Which unit of the computer is considered as the brain of the computer ?",
+    option1: "Memory unit",
+    option2: "Input unit",
+    option3: "CPU",
+    option4: "Output unit ",
+    ans: 3,
+
+},
+{
+    question:"Which device is required for the Internet connection?",
+    option1: "Modem",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Pen Drive",
+    ans: 1,
+
+},
+
+ 
+{
+    question:"What is the full form of PROM ?",
+    option1: "Program read-only memory",
+    option2: "Primary read-only memory",
+    option3: "Programmable read-only memory",
+    option4: "Program read-output memory",
+    ans: 3,
+
+},
+
+ 
+{
+    question:"Which of the following is an input device used to enter motion data in computers or other electronic devices ?",
+    option1: "Monitor",
+    option2: "Trackball",
+    option3: "Plotter",
+    option4: "Joystick",
+    ans: 2,
+
+},
+]
